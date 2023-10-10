179bb03921782f80219510958a79cfdc46256a17
Adel410 <adel.hadj410@gmail.com>
2023-10-10 04:23:45 -0700

    c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 /**
 * main - entry point
-* return: always 0 (success)
+* return - always 0 (success)
 */
 int main(void)
 {
-  printf("Size of a char : %lu bytes(s)\n", sizeof(char));
-  printf("Size of a char : %lu bytes(s)\n", sizeof(int));
-  printf("Size of a char : %lu bytes(s)\n", sizeof(long int));
-  printf("Size of a char : %lu bytes(s)\n", sizeof(long long int));
-  printf("Size of a char : %lu bytes(s)\n", sizeof(float));
-  return (0);
+printf("Size of a char : %lu bytes(s)\n", sizeof(char));
+printf("Size of a char : %lu bytes(s)\n", sizeof(int));
+printf("Size of a char : %lu bytes(s)\n", sizeof(long int));
+printf("Size of a char : %lu bytes(s)\n", sizeof(long long int));
+printf("Size of a char : %lu bytes(s)\n", sizeof(float));
+return (0);
 }
