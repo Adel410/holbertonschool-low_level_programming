2d8ad489aa63a181b12f6a2ec8670575f03348d8
Adel410 <adel.hadj410@gmail.com>
2023-10-11 01:21:59 -0700

    c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
-
-
+/**
+* main - print a text without using printf or puts functions
+*
+* Return - 1 always sucess
+*/
 
 int main(void)
 {
-char *s="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 while (*s) putchar(*s++);
 return (1);
 }
