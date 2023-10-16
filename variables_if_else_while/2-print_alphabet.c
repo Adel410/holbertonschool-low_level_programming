04d6158db9dea8132dae8025f977439a27cb8068
Adel410 <adel.hadj410@gmail.com>
2023-10-16 03:59:08 -0700

    alpha

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+char c, d;
+c = 'a';
+d = 'z';
+while (c != d)
+{
+putchar(c);
+c++;
+}
+return (0);
+}
+
