6ec30fcfec5126a836216b30a2cd6b10d3abe5fa
Adel410 <adel.hadj410@gmail.com>
2023-10-16 03:14:10 -0700

    else

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %d is %d and is 0\n", n, dernier_c);
 }
 else if (n > 5 && n != 0)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, dernier_c);
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 else if (n < 6 && n != 0)
 {
