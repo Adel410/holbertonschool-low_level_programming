3401b223587f242ae44418f52987a29450308499
Adel410 <adel.hadj410@gmail.com>
2023-10-17 07:31:37 -0700

    num

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_numbers - Print numbers from 0 to 10 using putchar.
+ * main - entry point
+ * Return: always 0 (success)
  */
 int main(void)
 {
-int n = 0;	
-while (n <= 9)
+int n = 0;
+while (n <= 10)
 {
 putchar('0' + n);
 n++;
