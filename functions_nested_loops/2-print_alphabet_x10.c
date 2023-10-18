0f1da6e301b5f7ee6816b6ea8a2d27a524e6035a
Adel410 <adel.hadj410@gmail.com>
2023-10-18 09:15:06 -0700

    compliqué

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * function print_alphabet_10x - print the alphabet 10 times on 10 different lines
+ * function print_alphabet_10x - print the alphabet 10
  *
  * Return: void
  */
@@ -9,13 +9,11 @@ void print_alphabet_x10(void)
 {
 	char c = 'a';
 	int i;
-	for (c = c; c <= 'z'; c++)
+	for (i = 1; 1 <= 10; i++)
 	{
-		_putchar(c);
-		for (i = 1; 1 <= 10; i++)
-		{
-			_putchar('\n');
-		}
+		for (c = c; c <= 'z'; c++)
+			_putchar(c);
 	}
+	_putchar('\n');
 return;
 }
