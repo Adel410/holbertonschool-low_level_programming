a508abc712571d01eb5ef73e57777132855330a4
Adel410 <adel.hadj410@gmail.com>
2023-10-18 02:40:52 -0700

    nested

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_10x - print the alphabet 10 times on 10 different lines
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	for (c = 1; c <= 10; c++)
+	{
+	for (c = c; c <= 'z'; c++)
+		{
+			_putchar(c);
+			_putchar('\n');
+		}
+	}
+return;
+}
