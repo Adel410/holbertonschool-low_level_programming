cd8228ec1ff313a5176b0930022fd81a7f1de8fc
Adel410 <adel.hadj410@gmail.com>
2023-10-18 08:58:48 -0700

    compliqué

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,12 @@ void print_alphabet_x10(void)
 {
 	char c = 'a';
 	int i;
-	for (i = 1; 1 <= 10; i++);
-	{	
 	for (c = c; c <= 'z'; c++)
 	{
-		_putchar(c);
-	}
+		for (i = 1; 1 <= 10; i++)
+		{
+			_putchar(c);
+		}
 	_putchar('\n');
 	}
 return;
