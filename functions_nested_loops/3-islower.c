5ca29ae9df8a1aa3c27ae32b8f35575e1e4f7e9d
Adel410 <adel.hadj410@gmail.com>
2023-10-18 09:40:48 -0700

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - check if the characters are in lowercase
+ * Return: 1 if true 0 if false
+ */
+
+int _islower(int c)
+{
+	if (int c == islower)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
