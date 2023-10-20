bbdabc690dd3126ee7489d7dcb1ef96c1152c4c7
Adel410 <adel.hadj410@gmail.com>
2023-10-20 06:58:53 -0700

    jack

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -11,11 +11,13 @@ void jack_bauer(void)
 	for (a = 0; a < 24; a++)
 	{
 		for (z = 0; z < 60; z++)
+		{
 			_putchar(a / 10 + '0');
 			_putchar(a % 10 + '0');
 			_putchar(':');
 			_putchar(z / 10 + '0');
 			_putchar(z % 10 + '0');
 			_putchar('\n');
+		}
 	}
 }
